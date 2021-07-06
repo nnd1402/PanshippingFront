cb65e441672bd42c3e6c6d9e6b04976537f40332
DEVK136\Panonit <zeklbike@gmail.com>
2021-07-06 15:30:13 +0200

    napravljen routmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RegistrationComponentComponent } from './registration-component/registration-component.component';
+
+const routes: Routes = [
+  { path: '', component: RegistrationComponentComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
