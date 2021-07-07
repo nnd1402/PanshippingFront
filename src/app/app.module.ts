062571456e4095655accf25f5102fdc6c0677758
DEVK136\Panonit <zeklbike@gmail.com>
2021-07-07 08:41:24 +0200

    app-routing modul izdvojen u zasebnu celinu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegistrationComponentComponent } from './registration-component/registration-component.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
-const routes: Routes = [
-  { path: '', component: RegistrationComponentComponent }
-];
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponentComponent
+    RegistrationComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
