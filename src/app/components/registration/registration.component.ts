5f13ef6795dd25536695a9b669cd36f0a749ae3e
DEVK136\Panonit <zeklbike@gmail.com>
2021-07-16 11:24:15 +0200

    -regex pattern za phone number u registration form ubacen u konstante constants.ts

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Validation } from './password-validator';
 import { RegistrationService } from '../../services/registration.service';
-import { EMPTY_STRING } from 'src/app/utility/constants';
+import { EMPTY_STRING, NUMBER_REGEXP } from 'src/app/utility/constants';
 
 @Component({
   selector: 'app-registration',
@@ -31,7 +31,7 @@ export class RegistrationComponent implements OnInit {
       confirmPassword: [EMPTY_STRING, Validators.required],
       address: [EMPTY_STRING, Validators.required],
       country: [EMPTY_STRING, Validators.required],
-      phone: [EMPTY_STRING, [Validators.required, Validators.pattern('^[0-9]*$')]],
+      phone: [EMPTY_STRING, [Validators.required, Validators.pattern(NUMBER_REGEXP)]],
     },
     {
       validators: [Validation.match('password', 'confirmPassword')]
