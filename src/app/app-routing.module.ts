24ae09f01c57369e06ef91fac556d620c8e35237
DEVK136\Panonit <zeklbike@gmail.com>
2021-07-19 09:05:29 +0200

    implementiran auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './services/auth.guard';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: 'registration', component: RegistrationComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'registration', component: RegistrationComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
+  // posto jos nemam UserComponent stavio sam AppComponent,
+  // ako neko unese /user u url redirektovace ga na login stranicu
+  { path: 'user', component: AppComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
