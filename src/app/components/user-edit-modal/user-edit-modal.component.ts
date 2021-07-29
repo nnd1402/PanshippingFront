0a0d16bee35272c03040c5944a36d0645048e168
DEVK136\Panonit <zeklbike@gmail.com>
2021-07-29 11:12:22 +0200

    -izbrisane izmene u projektu koje ne odgovaraju User module task-u

--- src/app/components/user-edit-modal/user-edit-modal.component.ts
+++ src/app/components/user-edit-modal/user-edit-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject, Input } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { AbstractControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { IUser } from '../../interfaces/user';
